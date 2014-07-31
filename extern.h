2567880aeb9fbc60cf79b66e46859743e0a254d1
kristaps <>
2014-07-31 14:51:34 +0000

    Understand an article's tags.

--- extern.h
+++ extern.h
@@ -1,3 +1,19 @@
+/*	$Id$ */
+/*
+ * Copyright (c) 2013 Kristaps Dzonsons <kristaps@bsd.lv>,
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
 #ifndef EXTERN_H
 #define EXTERN_H
 
@@ -15,6 +31,7 @@ struct	article {
 	time_t	 	 time; /* date of publication */
 	char		*article; /* nil-terminated entire article */
 	size_t		 articlesz; /* length of article */
+	char		*tags; /* any article tags */
 };
 
 #define	xrealloc realloc
