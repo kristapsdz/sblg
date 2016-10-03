8d6a0c614aca9baf2eea393c7f84153f7b795d69
kristaps <>
2016-10-03 21:10:20 +0000

    Complete JSON integration.

--- json.c
+++ json.c
@@ -66,6 +66,24 @@ json_quoted(const char *cp, FILE *f)
 }
 
 static void
+json_time(const char *key, time_t t, FILE *f)
+{
+
+	json_quoted(key, f);
+	fputc(':', f);
+	fprintf(f, "%lld", (long long)t);
+}
+
+static void
+json_text(const char *key, const char *text, FILE *f)
+{
+
+	json_quoted(key, f);
+	fputc(':', f);
+	json_quoted(text, f);
+}
+
+static void
 json_texthtml(const char *key, 
 	const char *text, const char *html, FILE *f)
 {
@@ -83,6 +101,31 @@ json_texthtml(const char *key,
 	fputc(':', f);
 	json_quoted(html, f);
 	fputc('}', f);
+}
+
+static void
+json_textlist(const char *key, const char *list, FILE *f)
+{
+	char	*token, *string, *tofree;
+	int	 first = 1;
+	
+	json_quoted("tags", f);
+	fputc(':', f);
+	fputc('[', f);
+
+	if (NULL != list) {
+		tofree = string = xstrdup(list);
+		while (NULL != (token = strsep(&string, " "))) {
+			if ('\0' == *token)
+				continue;
+			if ( ! first) 
+				fputc(',', f);
+			json_quoted(token, f);
+			first = 0;
+		}
+		free(tofree);
+	}
+	fputc(']', f);
 }
 
 int
@@ -121,7 +164,18 @@ json(XML_Parser p, int sz, char *src[], const char *dst, enum asort asort)
 
 	for (j = 0; j < sargsz; j++) {
 		fputc('{', f);
-
+		json_text("src", sargs[j].src, f);
+		fputc(',', f);
+		json_text("base", sargs[j].base, f);
+		fputc(',', f);
+		json_text("stripbase", 
+			sargs[j].stripbase, f);
+		fputc(',', f);
+		json_text("striplangbase", 
+			sargs[j].striplangbase, f);
+		fputc(',', f);
+		json_time("time", sargs[j].time, f);
+		fputc(',', f);
 		json_texthtml("title", 
 			sargs[j].titletext, 
 			sargs[j].title, f);
@@ -136,7 +190,9 @@ json(XML_Parser p, int sz, char *src[], const char *dst, enum asort asort)
 		fputc(',', f);
 		json_texthtml("article", NULL,
 			sargs[j].article, f);
-
+		fputc(',', f);
+		json_textlist("tags", 
+			sargs[j].tags, f);
 		fputc('}', f);
 		if (j < sargsz - 1)
 			fputc(',', f);
