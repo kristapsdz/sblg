a17bae2fbd6252e8ec256d25bb7364a987816d75
kristaps <>
2017-03-18 08:33:46 +0000

    Start to add JSON-possible output for -l.

--- listtags.c
+++ listtags.c
@@ -19,37 +19,80 @@
 #include <assert.h>
 #include <ctype.h>
 #include <expat.h>
+#include <search.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "extern.h"
 
-int
-listtags(XML_Parser p, int sz, char *src[])
+/*
+ * Print article-major ordering.
+ * This prints the tags belonging to each article.
+ */
+static void
+dolist(const struct article *sargs, size_t sargsz, int json)
 {
-	size_t		 j, k, sargsz = 0;
-	int		 i, rc = 0;
-	struct article	*sargs = NULL;
+	size_t	 	 i, j;
 	const char	*cp;
 
-	for (i = 0; i < sz; i++) 
-		if ( ! sblg_parse(p, src[i], &sargs, &sargsz))
-			goto out;
+	if ( ! hcreate(256)) {
+		perror(NULL);
+		exit(EXIT_FAILURE);
+	}
 
-	for (j = 0; j < sargsz; j++)
-		for (k = 0; k < sargs[j].tagmapsz; k++) {
-			for (cp = sargs[j].tagmap[k]; '\0' != *cp; cp++) {
+	for (i = 0; i < sargsz; i++) {
+		if (json)
+			printf("{\"src\": \"%s\", \n"
+			       " \"tags\": [", sargs[i].src);
+		for (j = 0; j < sargs[i].tagmapsz; j++) {
+			if (json && j > 0)
+				putchar(',');
+			if (json)
+				putchar('"');
+			for (cp = sargs[i].tagmap[j]; '\0' != *cp; cp++) {
 				if ('\\' == cp[0] && ' ' == cp[1])
 					continue;
 				putchar(*cp);
 			}
-			printf("\t%s\n", sargs[j].src);
+			if (json)
+				putchar('"');
+			else
+				printf("\t%s\n", sargs[i].src);
+		}
+		if (json && i < sargsz - 1)
+			puts("]},");
+		else if (json)
+			puts("]}");
+	}
+}
+
+int
+listtags(XML_Parser p, int sz, char *src[], int json, int reverse)
+{
+	size_t		 sargsz = 0;
+	int		 i;
+	struct article	*sargs = NULL;
+
+	/* First run the initial parse of all files. */
+
+	for (i = 0; i < sz; i++) 
+		if ( ! sblg_parse(p, src[i], &sargs, &sargsz)) {
+			sblg_free(sargs, sargsz);
+			return(0);
 		}
 
-	rc = 1;
-out:
+	/* Now actually emit the listings. */
+
+	if (json)
+		puts("{[");
+	dolist(sargs, sargsz, json);
+	if (json)
+		puts("]}");
+
+	/* Cleanup and exit. */
+
 	sblg_free(sargs, sargsz);
-	return(rc);
+	return(1);
 }
 
