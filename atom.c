4a6682e91d5752898452f9efdbe0e58449516d11
kristaps <>
2019-03-18 12:35:40 +0000

    Add data-sblg-forall to simplify Atom creation.  Also add lots of documentation and remove superfluous declarations.

--- atom.c
+++ atom.c
@@ -32,38 +32,34 @@
 
 #include "extern.h"
 
+/*
+ * Holds all information on a pending parse of the Atom template.
+ */
 struct	atom {
-	FILE		*f;
-	const char	*src;
-	const char	*dst;
-	XML_Parser	 p;
-	char		 domain[MAXHOSTNAMELEN];
-	char		 path[MAXPATHLEN];
-	struct article	*sargs;
-	size_t		 spos;
-	size_t		 sposz;
-	int		 hasid;
-	size_t		 stack;
+	FILE		*f; /* atom template input */
+	const char	*src; /* template file */
+	const char	*dst; /* output or "-" for stdout */
+	XML_Parser	 p; /* parser instance */
+	char		 domain[MAXHOSTNAMELEN]; /* ...or localhost */
+	char		 path[MAXPATHLEN]; /* path in link */
+	struct article	*sargs; /* articles */
+	size_t		 spos; /* current article */
+	size_t		 sposz; /* article length */
+	size_t		 stack; /* position in discard stack */
 };
 
-static	void	atomprint(FILE *f, const struct atom *arg, 
-			int altlink, int striplink, int content, 
-			const struct article *src);
-static	void	entry_begin(void *userdata, const XML_Char *name, 
-			const XML_Char **atts);
-static	void	entry_empty(void *userdata, const XML_Char *name);
-static	void	entry_end(void *userdata, const XML_Char *name);
-static	void	id_begin(void *userdata, const XML_Char *name, 
-			const XML_Char **atts);
-static	void	id_end(void *userdata, const XML_Char *name);
-static	void	tmpl_begin(void *userdata, const XML_Char *name, 
-			const XML_Char **atts);
-static	void	tmpl_end(void *userdata, const XML_Char *name);
-static	void	tmpl_text(void *userdata, 
-			const XML_Char *s, int len);
-static	void	up_begin(void *userdata, const XML_Char *name, 
-			const XML_Char **atts);
-static	void	up_end(void *userdata, const XML_Char *name);
+/* Functions may be called out-of-order of definitions. */
+
+static void entry_begin(void *, const XML_Char *, const XML_Char **);
+static void entry_empty(void *, const XML_Char *);
+static void entry_end(void *, const XML_Char *);
+static void id_begin(void *, const XML_Char *, const XML_Char **);
+static void id_end(void *, const XML_Char *);
+static void tmpl_begin(void *, const XML_Char *, const XML_Char **);
+static void tmpl_end(void *, const XML_Char *);
+static void tmpl_text(void *, const XML_Char *, int);
+static void up_begin(void *, const XML_Char *, const XML_Char **);
+static void up_end(void *, const XML_Char *);
 
 static void
 atomputs(FILE *f, const char *cp)
@@ -125,6 +121,9 @@ atomprint(FILE *f, const struct atom *arg, int altlink,
 	}
 }
 
+/*
+ * Given the Atom template "templ", fill in entries etc.
+ */
 int
 atom(XML_Parser p, const char *templ, int sz, 
 	char *src[], const char *dst, enum asort asort)
@@ -146,9 +145,12 @@ atom(XML_Parser p, const char *templ, int sz,
 	sargs = NULL;
 	sargsz = 0;
 
-	getdomainname(larg.domain, MAXHOSTNAMELEN);
-	if ('\0' == larg.domain[0])
+	if (getdomainname(larg.domain, MAXHOSTNAMELEN) < 0) {
+		warn("getdomainname");
+		goto out;
+	} else if ('\0' == larg.domain[0])
 		strlcpy(larg.domain, "localhost", MAXHOSTNAMELEN);
+
 	strlcpy(larg.path, "/", MAXPATHLEN);
 
 	for (i = 0; i < sz; i++)
@@ -243,7 +245,7 @@ tmpl_begin(void *userdata,
 	struct atom	 *arg = userdata;
 	time_t		  t;
 	char		  buf[1024];
-	int		  altlink, content, striplink;
+	int		  altlink, content, striplink, useall = 0;
 	const char	 *start;
 	char		 *cp;
 	struct tm	 *tm;
@@ -349,15 +351,25 @@ tmpl_begin(void *userdata,
 			striplink = xmlbool(attp[1]);
 		else if (0 == strcasecmp(*attp, "data-sblg-content"))
 			content = xmlbool(attp[1]);
+		else if (0 == strcasecmp(*attp, "data-sblg-forall"))
+			useall = xmlbool(attp[1]);
 
 	arg->stack++;
 	XML_SetDefaultHandlerExpand(arg->p, NULL);
-	if (arg->sposz > arg->spos) {
+	if (arg->sposz > arg->spos && useall) {
+		while (arg->spos < arg->sposz) {
+			fprintf(arg->f, "<%s>", name);
+			atomprint(arg->f, arg, altlink, striplink,
+				content, &arg->sargs[arg->spos++]);
+			fprintf(arg->f, "</%s>", name);
+		}
+		XML_SetDefaultHandlerExpand(arg->p, NULL);
+		XML_SetElementHandler(arg->p, entry_begin, entry_end);
+	} else if (arg->sposz > arg->spos) {
 		fprintf(arg->f, "<%s>", name);
 		XML_SetDefaultHandlerExpand(arg->p, NULL);
 		XML_SetElementHandler(arg->p, entry_begin, entry_end);
-		atomprint(arg->f, arg, altlink, striplink,
-			content, &arg->sargs[arg->spos++]);
+		fprintf(arg->f, "</%s>", name);
 	} else {
 		XML_SetElementHandler(arg->p, entry_begin, entry_empty);
 	}
@@ -386,10 +398,13 @@ static void
 id_end(void *userdata, const XML_Char *name)
 {
 	struct atom	*arg = userdata;
+	const char	*dst;
+
+	dst = (0 == strcmp(arg->dst, "-")) ? "" : arg->dst;
 
 	if (0 == strcasecmp(name, "id") && 0 == --arg->stack) {
 		fprintf(arg->f, "tag:%s,2013:%s/%s</%s>", 
-			arg->domain, arg->path, arg->dst, name);
+			arg->domain, arg->path, dst, name);
 		XML_SetElementHandler(arg->p, tmpl_begin, tmpl_end);
 		XML_SetDefaultHandlerExpand(arg->p, tmpl_text);
 	}
@@ -413,7 +428,6 @@ entry_end(void *userdata, const XML_Char *name)
 	struct atom	*arg = userdata;
 
 	if (0 == strcasecmp(name, "entry") && 0 ==  --arg->stack) {
-		fprintf(arg->f, "</%s>", name);
 		XML_SetElementHandler(arg->p, tmpl_begin, tmpl_end);
 		XML_SetDefaultHandlerExpand(arg->p, tmpl_text);
 	}
