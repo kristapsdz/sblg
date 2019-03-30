2a58061d79b9e1134cef6dbf97c71954d0e1229a
kristapsdz <kristaps@bsd.lv>
2019-03-30 14:39:34 +0000

    Usage of -l (can have another -l).

--- main.c
+++ main.c
@@ -210,7 +210,7 @@ usage:
 	fprintf(stderr, 
 		"usage: %s [-o file] [-t templ] -c file...\n"
 		"       %s [-o file] [-t templ] [-s sort] -a file...\n"
-		"       %s [-jr] -l file...\n"
+		"       %s [-jlr] -l file...\n"
 		"       %s [-t templ] [-s sort] -L file...\n"
 		"       %s [-o file] [-s sort] -j file...\n"
 		"       %s [-o file] [-t templ] [-s sort] -C file...\n"
