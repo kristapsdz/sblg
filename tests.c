008d6f067469afdd130cd63156e39cc76492555f
kristapsdz <kristaps@bsd.lv>
2020-07-23 14:30:00 +0000

    Newest oconfigure.

--- tests.c
+++ tests.c
@@ -116,6 +116,41 @@ main(void)
 	return(0);
 }
 #endif /* TEST_EXPLICIT_BZERO */
+#if TEST_FTS
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fts.h>
+
+int
+main(void)
+{
+	const char	*argv[2];
+	FTS		*ftsp;
+	FTSENT		*entry;
+
+	argv[0] = ".";
+	argv[1] = (char *)NULL;
+
+	ftsp = fts_open((char * const *)argv,
+	    FTS_PHYSICAL | FTS_NOCHDIR, NULL);
+
+	if (ftsp == NULL)
+		return 1;
+
+	entry = fts_read(ftsp);
+
+	if (entry == NULL)
+		return 1;
+
+	if (fts_set(ftsp, entry, FTS_SKIP) != 0) 
+		return 1;
+
+	if (fts_close(ftsp) != 0)
+		return 1;
+
+	return 0;
+}
+#endif /* TEST_FTS */
 #if TEST_GETEXECNAME
 #include <stdlib.h>
 
@@ -380,6 +415,22 @@ main(void)
 	return setresuid(-1, -1, -1) == -1;
 }
 #endif /* TEST_SETRESUID */
+#if TEST_SHA2_H
+#include <sys/types.h>
+#include <sha2.h>
+
+int main(void)
+{
+	SHA2_CTX ctx;
+	char result[SHA256_DIGEST_STRING_LENGTH];
+
+	SHA256Init(&ctx);
+	SHA256Update(&ctx, (const unsigned char *)"abcd", 4);
+	SHA256End(&ctx, result);
+
+	return 0;
+}
+#endif /* TEST_SHA2_H */
 #if TEST_SOCK_NONBLOCK
 /*
  * Linux doesn't (always?) have this.
